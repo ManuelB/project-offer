2b93ecc39a15ec22266e91aed0fd390cfeb38bb9
Manuel Blechschmidt <m.blechschmidt@mobizcorp.de>
2020-10-29 09:34:47 +0100

    Only show project amount for last 30 days, enhanced search for all

--- frontend/src/main/webapp/incentergy/projectoffer/view/Master.controller.js
+++ frontend/src/main/webapp/incentergy/projectoffer/view/Master.controller.js
@@ -1,37 +1,11 @@
 sap.ui.define([
 	"incentergy/base/view/AbstractMasterController",
 	'sap/ui/model/Filter',
-	'sap/ui/model/FilterOperator',
-	'sap/ui/model/FilterType'
-], function (AbstractMasterController, Filter, FilterOperator, FilterType) {
+	'sap/ui/model/FilterOperator'
+], function (AbstractMasterController, Filter, FilterOperator) {
 	"use strict";
 
 	return AbstractMasterController.extend("incentergy.projectoffer.view.Master", {
-		
-		onInit: function () {
-			AbstractMasterController.prototype.onInit.apply(this, arguments);
-			this.oRouter = this.getOwnerComponent().getRouter();
-			
-			this.oRouter.attachRouteMatched(function(oEvent) {
-				if(oEvent.getParameter("name") == "search") {
-					this.onRouteSearchMatched(oEvent);
-				}
-			}.bind(this));
-			
-		},
-		onRouteSearchMatched: function(oEvent) {
-			let sQuery = oEvent.getParameter("arguments").query;
-			
-			let aQueryParts = sQuery.split(/&/).map(s => s.split(/=/)).map(a => [a[0].replace("_", "/"), a[1]]);
-			
-			let aFilters = [];
-			for(let aPart of aQueryParts) {
-				let sField = aPart[0];
-				let sValue = aPart[1];
-				aFilters.push(new Filter({path: sField, operator: FilterOperator.EQ, value1: sValue}));
-			}
-			this.byId("projectofferTable").getBinding("items").filter(aFilters, FilterType.Application);
-		},
 		getEntityName: function() {
 			return "projectoffer";
 		},
